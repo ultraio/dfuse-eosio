8e7e664ce4a1dfea990b60e8956126083e6c0c17
Rami James <ramijames@gmail.com>
2021-07-12 15:05:40 +0300

    Updated footer links

--- eosq/src/i18n/en/footer.i18n.ts
+++ eosq/src/i18n/en/footer.i18n.ts
@@ -3,8 +3,8 @@ export default {
     telegram: "",
     aboutDfuse: "",
     copyright: "© 2018-{{year}} Ultra. All Rights Reserved.",
-    privacyPolicy: "Privacy Policy",
-    privacyPolicyLink: "https://ultra.io/documents/privacy-policy/recruitment-privacy-policy.pdf",
+    privacyPolicy: "",
+    privacyPolicyLink: "",
     termsOfServices: "Terms of Service",
     termsOfServicesLink: "https://ultra.io/terms"
   }
