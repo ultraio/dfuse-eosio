1da974fa5e452be9fe770b6d090449164b1c2b38
Duncan Dam <duncan.dam@ultra.io>
2024-05-29 12:06:27 +0700

    update

--- dgraphql/resolvers/trxcache.go
+++ dgraphql/resolvers/trxcache.go
@@ -8,23 +8,22 @@ import (
 
 type TrxCache struct {
 	capacity int
-	data     map[string]bool
+	data     map[string]*list.Element
 	order    *list.List
 }
 
 func NewTrxCache(capacity int) *TrxCache {
 	return &TrxCache{
 		capacity: capacity,
-		data:     make(map[string]bool),
+		data:     make(map[string]*list.Element),
 		order:    list.New(),
 	}
 }
 
 func (c *TrxCache) Put(key string) {
-	if _, exists := c.data[key]; exists {
-		// If key already exists, update value and move it to the front
-		c.data[key] = true
-		c.moveToFront(key)
+	if ele, exists := c.data[key]; exists {
+		// If key already exists, move it to the front
+		c.order.MoveToFront(ele)
 		return
 	}
 
@@ -33,18 +32,9 @@ func (c *TrxCache) Put(key string) {
 		c.evictOldest()
 	}
 
-	// Add new entry to the map and the front of the list
-	c.data[key] = true
-	c.order.PushFront(key)
-}
-
-func (c *TrxCache) Get(key string) (interface{}, bool) {
-	if value, exists := c.data[key]; exists {
-		// Move accessed item to the front
-		c.moveToFront(key)
-		return value, true
-	}
-	return nil, false
+	// Add new entry to the front of the list and to the map
+	ele := c.order.PushFront(key)
+	c.data[key] = ele
 }
 
 func (c *TrxCache) Exists(key string) bool {
@@ -58,15 +48,5 @@ func (c *TrxCache) evictOldest() {
 	if oldest != nil {
 		c.order.Remove(oldest)
 		delete(c.data, oldest.Value.(string))
-	}
-}
-
-func (c *TrxCache) moveToFront(key string) {
-	// Move accessed or updated key to the front of the list
-	for e := c.order.Front(); e != nil; e = e.Next() {
-		if e.Value.(string) == key {
-			c.order.MoveToFront(e)
-			break
-		}
 	}
 }
